40fbe5b7523c8dd7adb48014b5dcbe30914f63c6
Ethan Moss <emoss14@georgefox.edu>
2018-03-18 23:25:08 -0700

    Added views, made some progress on bootstrap navbar

---
+++ emoss_website/config.py
@@ -0,0 +1,5 @@
+import os
+import socket
+
+# Config Settings
+basedir = os.path.abspath(os.path.dirname(__file__))
--- emoss_website/views.py
+++ emoss_website/views.py
@@ -7,5 +7,24 @@ from flask import render_template, url_for, request, Response
 
 @app.route('/')
 def home_page():
-    return "Hello, World!"
+    return render_template('home.jinja2.html')
 
+
+@app.route('/about')
+def about_page():
+    return "About Page: Coming Soon!"
+
+
+@app.route('/blog')
+def blog_page():
+    return "Blog: Coming Soon!"
+
+
+@app.route('/resume')
+def resume_page():
+    return "Resume: Coming Soon!"
+
+
+@app.route('/contact')
+def contact_page():
+    return "Contact Page: Coming Soon!"
